cc4fc3d24ecbc117a1ff461a586b4ef820d11c1c
Aaron Schraufnagel <aaronts@uw.edu>
2015-03-04 15:38:29 -0800

    Refined enemy spawn zones on level 1
    Working on level 2 enemy spawning next

--- Level1.ts
+++ Level1.ts
@@ -27,12 +27,11 @@
             layer.resizeWorld();
             super.setLayer(layer);
             
-            enemiesTotal = 15;
+            enemiesTotal = 12;
             super.setEnemiesTotal(enemiesTotal);
 
-            enemyLocationsX = [this.game.rnd.integerInRange(450, 815), this.game.rnd.integerInRange(1215, 1840), this.game.rnd.integerInRange(3119, 3518), this.game.rnd.integerInRange(3519, 3729),
-                this.game.rnd.integerInRange(3730, 4047), this.game.rnd.integerInRange(6447, 7000), this.game.rnd.integerInRange(7001, 7790), this.game.rnd.integerInRange(7791, 8368),
-                this.game.rnd.integerInRange(8369, 8752), this.game.rnd.integerInRange(11600, 12100), this.game.rnd.integerInRange(12101, 12600), this.game.rnd.integerInRange(12601, 13100),
+            enemyLocationsX = [this.game.rnd.integerInRange(450, 815), this.game.rnd.integerInRange(1215, 1840), this.game.rnd.integerInRange(3519, 3729),
+                this.game.rnd.integerInRange(3730, 4047), this.game.rnd.integerInRange(6447, 7000), this.game.rnd.integerInRange(8369, 8752), this.game.rnd.integerInRange(11600, 12100), this.game.rnd.integerInRange(12101, 12600), this.game.rnd.integerInRange(12601, 13100),
                 this.game.rnd.integerInRange(13101, 13965), this.game.rnd.integerInRange(15700, 16150), this.game.rnd.integerInRange(16151, 16560)];
             enemyLocationsY = [373, 373, 129, 373, 208, 192, 192, 96, 32, 192, 192, 192, 192, 208, 208];
             super.setEnemyLocations(enemyLocationsX, enemyLocationsY);
